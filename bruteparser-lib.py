618fc82b8123efc6f2bbba0e9c5f42615031fb1b
Ted Kim <ted.kim03@gmail.com>
2019-03-01 16:20:29 -0500

    Create bruteparser-lib.py
    extensible version of the bruteparser

---
+++ bruteparser-lib.py
@@ -0,0 +1,36 @@
+import docx2txt
+from docx import *
+import re
+import subprocess
+
+class docparse:
+
+        def __init__(self, filename):
+                self.filename = filename
+                self.filetype = re.search(r'\.{1}\w+', filename).group(0)[1:]
+                self.acceptedType = ['doc', 'docx']
+                if self.filetype in self.acceptedType:
+                        self.status = True
+                else:
+                        self.status = False
+        def __call__(self):
+                return
+
+        def parsedocx(self):
+                docx = docx2txt.process(self.filename)
+                return docx
+
+        def parsedoc(self):
+                cmd = "antiword '{}'".format(self.filename)
+                doc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+                return doc.stdout.decode('utf-8')
+
+        def parse(self):
+                if self.status == True:
+                        if self.filetype == 'doc':
+                                result = self.parsedoc()
+                        elif self.filetype == 'docx':
+                                result = self.parsedocx()
+                        return result
+                else:
+                        return "The uploaded file ({}) is not a valid filetype ({})".format(self.filename, str(self.acceptedType))
